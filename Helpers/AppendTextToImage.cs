d48787cdc73fc3da06fba46cad1bd65d9c7641ce
Salvador M <chava_r17@hotmail.com>
2025-01-13 18:46:39 -0600

    Update the template for the notification

--- Helpers/AppendTextToImage.cs
+++ Helpers/AppendTextToImage.cs
@@ -13,10 +13,10 @@ namespace Sicem_Blazor.Helpers;
 
 public class AppendTextToImage
 {
-    public float TextPadding {get;set;} = 190f;
+    public float TextPadding {get;set;} = 80f;
     public string TextFont {get;set;} = "Arial";
-    public float TextFontSize {get;set;} = 40f;
-    public string TextColorHex {get;set;} = "#3a5a52FF";
+    public float TextFontSize {get;set;} = 44f;
+    public string TextColorHex {get;set;} = "#342222FF";
     private Font font;
 
 
@@ -62,11 +62,11 @@ public class AppendTextToImage
         var rect = TextMeasurer.MeasureSize(message, options);
 
         var richTextOptions = new RichTextOptions(font);
-        richTextOptions.Origin = new PointF(TextPadding, TextPadding * 1.1f);
+        richTextOptions.Origin = new PointF(TextPadding, TextPadding * 3f);
         richTextOptions.TextAlignment = TextAlignment.Center;
         richTextOptions.TextJustification = TextJustification.None;
         richTextOptions.WrappingLength = image.Width - (TextPadding * 1.9f);
-        richTextOptions.LineSpacing = 1.25f;
+        richTextOptions.LineSpacing = 1.4f;
 
         // * draw the text into the image
         var pointF = new PointF(TextPadding, TextPadding + 10);

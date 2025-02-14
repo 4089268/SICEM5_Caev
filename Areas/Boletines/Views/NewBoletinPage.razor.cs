7a7d0348cd6312964d0b2622088310d3e70bef3f
Salvador M <chava_r17@hotmail.com>
2025-02-13 23:28:21 -0600

    Implemented store boletines

--- Areas/Boletines/Views/NewBoletinPage.razor.cs
+++ Areas/Boletines/Views/NewBoletinPage.razor.cs
@@ -12,10 +12,10 @@ using Syncfusion.Blazor.Grids;
 using Syncfusion.Blazor.Charts;
 using MatBlazor;
 using Sicem_Blazor.Services;
-using Sicem_Blazor.Services.PagoLinea;
 using Sicem_Blazor.Models;
-using Sicem_Blazor.Models.PagoLinea;
 using Sicem_Blazor.Data;
+using Sicem_Blazor.Boletines.Services;
+using Sicem_Blazor.Boletines.Models;
 
 namespace Sicem_Blazor.Boletines.Views
 {
@@ -35,13 +35,11 @@ namespace Sicem_Blazor.Boletines.Views
         public ILogger<NewBoletinPage> Logger {get;set;} = default!;
 
         [Inject]
-        public NavigationManager NavigationManager1 {get;set;} = default!;
+        public IBoletinService BoletinService {get;set;} = default!;
 
-
-        private SfGrid<ResumeOffice> DataGrid {get;set;}
         private bool busyDialog = false;
         private OprBoletin Boletin {get;set;}
-        private string MensajeString {get;set;} = "Hola mundo";
+        private string MensajeString {get;set;} = "";
         private List<Destinatario> Destinatarios {get;set;}
         private List<BoletinMensaje> AttachedFiles {get;set;}
 
@@ -54,7 +52,7 @@ namespace Sicem_Blazor.Boletines.Views
 
             Boletin = new OprBoletin
             {
-                Titulo = "Nuevo Boletin " + DateTime.Now.ToShortDateString()
+                Titulo = "Boletin " + DateTime.Now.ToString("yyyyMMddHHmm")
             };
 
             AttachedFiles = new();
@@ -63,7 +61,7 @@ namespace Sicem_Blazor.Boletines.Views
         private async Task GoBackClick()
         {
             await Task.CompletedTask;
-            NavigationManager1.NavigateTo("/Boletines");
+            NavManager.NavigateTo("/Boletines");
         }
 
         private async Task LoadFilePhones(InputFileChangeEventArgs e)
@@ -73,26 +71,45 @@ namespace Sicem_Blazor.Boletines.Views
             
             // * proccesing the file
             IBrowserFile browserFile = e.File;
-            Console.WriteLine($"Archivo seleccionado {browserFile.Name}");
+            this.Logger.LogDebug("Archivo seleccionado {name}", browserFile.Name);
             var _tmpPath = string.Format("{0}/tmp_{1}.{2}", System.IO.Path.GetTempPath(), Guid.NewGuid().ToString(), browserFile.Name.Split(".").Last());
-            using (FileStream fs = new FileStream(_tmpPath, System.IO.FileMode.CreateNew)){
-                using(var stream = browserFile.OpenReadStream()){
+            using (FileStream fs = new FileStream(_tmpPath, System.IO.FileMode.CreateNew))
+            {
+                using(var stream = browserFile.OpenReadStream())
+                {
                     await stream.CopyToAsync(fs);
                 }
             }
-            Console.WriteLine($"Archivo generado: {_tmpPath}");
+            this.Logger.LogDebug("Archivo generado: {path}", _tmpPath);
             var procesarExcel = new ProcesarExcel();
-            var _datos = procesarExcel.ToDataTableFirstCol(_tmpPath);
+            var _datos = procesarExcel.ToDataTable(_tmpPath,2);
+            this.Logger.LogDebug("Total filas: {total}", _datos.Rows.Count);
 
             // * get the phone numbers
-            var tmpListaTelefonos = new List<long>();
+            var tmpListaDestinatarios = new List<Destinatario>();
             foreach(DataRow row in _datos.Rows)
             {
                 var telefono = Convert.ToInt64(row[0]);
-                if(!tmpListaTelefonos.Contains(telefono))
+                var lada = 52;
+                try
                 {
-                    tmpListaTelefonos.Add(telefono);
+                    lada = Convert.ToInt32(row[1]);
+                }catch(Exception)
+                {
+                    //
                 }
+
+                if(tmpListaDestinatarios.Select(item=> item.Telefono).Contains(telefono))
+                {
+                    continue;
+                }
+                tmpListaDestinatarios.Add( new Destinatario
+                {
+                    BoletinId = Boletin.Id,
+                    Lada = lada.ToString(),
+                    Telefono = telefono,
+                    Titulo = $"+{lada} {telefono}"
+                });
             }
 
             // * append the new phone numbers
@@ -100,22 +117,12 @@ namespace Sicem_Blazor.Boletines.Views
             {
                 Destinatarios = new List<Destinatario>();
             }
-
-            // * prevent add duplicated phones
-            var phonesToAdd = tmpListaTelefonos.Select(phone =>
-                new Destinatario {
-                    BoletinId = Boletin.Id,
-                    Titulo = $"+52 {phone}",
-                    Telefono = phone,
-                    Lada = "52"
-                }
-            );
-            Destinatarios.AddRange(phonesToAdd);
+            Destinatarios.AddRange(tmpListaDestinatarios);
 
             busyDialog = false;
         }
 
-        private async Task UploadAttacFile(InputFileChangeEventArgs e)
+        private async Task UploadAttachFile(InputFileChangeEventArgs e)
         {
             busyDialog = true;
             await Task.Delay(100);
@@ -156,6 +163,34 @@ namespace Sicem_Blazor.Boletines.Views
             busyDialog = false;
         }
 
-    }
+        private async Task SaveClick()
+        {
+            var boletinID = await this.BoletinService.AlmacenarBoletin(BoletinDTO.FromEntity(this.Boletin));
 
+            if(string.IsNullOrEmpty(this.MensajeString))
+            {
+                await this.BoletinService.StoreBoletinMensaje(boletinID, new BoletinMensaje
+                    {
+                        BoletinId = boletinID,
+                        EsArchivo = false,
+                        Mensaje = this.MensajeString,
+                        CreatedAt = DateTime.Now
+                    }
+                );
+            }
+            foreach(var fileAttach in AttachedFiles)
+            {
+                await this.BoletinService.StoreBoletinMensaje(boletinID, fileAttach);
+            }
+
+
+            foreach(var dest in Destinatarios)
+            {
+                await this.BoletinService.StoreBoletinDestinatatio(boletinID, dest);
+            }
+
+            this.Toaster.Add("Boletin Registrado!", MatToastType.Info);
+            await GoBackClick();
+        }
+    }
 }

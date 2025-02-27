f369978bdbc128f170109811a0a5e7fb5e6d99e5
Salvador Rangel <chava_r17@hotmail.com>
2025-02-27 10:23:13 -0600

    Create background service for fetching  the income data.

--- Areas/SeguimientoCobros/Data/SeguimientoCobroServiceCollectionExtension.cs
+++ Areas/SeguimientoCobros/Data/SeguimientoCobroServiceCollectionExtension.cs
@@ -6,10 +6,10 @@ namespace Sicem_Blazor.SeguimientoCobros.Data;
 
 public static class SeguimientoCobroServiceCollectionExtension
 {
-
     public static void AddSeguimientoCobroServices(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
         serviceCollection.AddTransient<SeguimientoCobroService>();
         serviceCollection.AddTransient<SeguimientoCobroMapJsInterop>();
+        serviceCollection.AddHostedService<IncomeBackgroundService>();
     }
 }
